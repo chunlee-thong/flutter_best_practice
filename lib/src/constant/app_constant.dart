461c894dedd9474630352d58a04b2ab20b82b8bd
Chunlee Thong <chunlee.thong@gmail.com>
2021-06-25 10:10:16 +0700

    remove unnecessary code

--- lib/src/constant/app_constant.dart
+++ lib/src/constant/app_constant.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../models/others/language_model.dart';
-
 class AppConstant {}
 
 const Locale EN_LOCALE = Locale('en', 'US');
@@ -13,19 +11,6 @@ class ErrorMessage {
   static const CONNECTION_ERROR = "Error connecting to server. Please check your internet connection or Try again later!";
   static const TIMEOUT_ERROR = "Connection timeout. Please check your internet connection or Try again later!";
 }
-
-const List<LanguageModel> APP_LOCALES = [
-  const LanguageModel(
-    locale: EN_LOCALE,
-    name: "English",
-    image: "https://ecolebranchee.com/wp-content/uploads/2015/04/etats-unis-drapeau.gif",
-  ),
-  const LanguageModel(
-    locale: KH_LOCALE,
-    name: "ខ្មែរ",
-    image: "https://wonderfulengineering.com/wp-content/uploads/2015/08/Cambodia-flag-3.gif",
-  ),
-];
 
 class HttpMethod {
   HttpMethod._();
